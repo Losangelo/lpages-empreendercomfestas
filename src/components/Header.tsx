6c06e268f7b7c906cdbf0f3050bdc0990fa93979
Losangelo <losangelo@gmail.com>
2021-04-28 10:24:43 -0300

    Update Login provider

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 import { NextPage } from 'next'
-
 import Logo from './Logo'
 import Menu from './Menu'
 
 const Header: NextPage = () => (
-  <div className="container max-w-screen-2xl mx-auto bg-red-900 bg-opacity-10 y-75 p-2 flex justify-between items-center fixed z-auto top-0 right-0">
+  <div className="bg-red-900 bg-opacity-10 flex container max-w-screen-2xl mx-auto y-75 p-2 justify-between items-center fixed z-auto top-0 right-0">
     <Logo />
     <Menu />
   </div>
